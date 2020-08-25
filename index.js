342176a5614137bf1fb6f1741b9b1488de75457b
Viorel Robu <wwjd.vio@gmail.com>
2020-08-25 07:23:12 +0300

    first get and post requests

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+// apply middleware to all requests
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
         <div>
-            <form>
-                <input placeholder="email">
-                <input placeholder="password">
-                <input placeholder="password confirmation">
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <input name="passwordConfirmation" placeholder="password confirmation">
                 <button>Sign Up</button>
             </form>
         </div>
     `);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account created!');
 });
 
 app.listen(3000, () => {
