1cf25d5c2b4bba31450a6f29d685e5bfab5f12a6
Viorel Robu <wwjd.vio@gmail.com>
2020-09-10 19:39:42 +0300

    added styles to user routes, started products

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
+
 // apply middleware to all requests
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
         keys: ['hjj123ka94bkag7abvhh??u45%$234$%%^^']
@@ -12,6 +15,7 @@ app.use(cookieSession({
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
     console.log('Listening!');
