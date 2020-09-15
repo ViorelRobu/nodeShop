6e539581771f8ac2a6aa14f3384c9b614abe5e83
Viorel Robu <wwjd.vio@gmail.com>
2020-09-15 22:43:56 +0300

    products images and admin products listing

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+
+const { handleErrors }= require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -11,11 +12,11 @@ router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', [ requireEmail, requirePassword, requirePasswordConfirmation ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.send(signupTemplate({ req, errors }));
-    }
+router.post(
+    '/signup', 
+    [ requireEmail, requirePassword, requirePasswordConfirmation ], 
+    handleErrors(signupTemplate),
+    async (req, res) => {
 
     const { email,  password } = req.body;
 
@@ -38,12 +39,12 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', [ requireEmailExists, requireValidPasswordForUser ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.send(signinTemplate({ req, errors }));
-    }
-
+router.post(
+    '/signin', 
+    [ requireEmailExists, requireValidPasswordForUser ], 
+    handleErrors(signinTemplate),
+    async (req, res) => {
+        
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({
